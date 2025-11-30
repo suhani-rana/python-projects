c7b6b5b28efbd63f0e8064f8b61404e66bac1f4c
Suhani Rana <ranasuhani45@gmail.com>
2025-11-30 17:58:58 +0530

    added calculator project

---
+++ calculator/calc.py
@@ -0,0 +1,33 @@
+def calculator():
+    print("Simple Calculator")
+    print("Choose an operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Please enter valid numbers!")
+        return
+
+    if choice == '1':
+        print("Result:", num1 + num2)
+    elif choice == '2':
+        print("Result:", num1 - num2)
+    elif choice == '3':
+        print("Result:", num1 * num2)
+    elif choice == '4':
+        if num2 == 0:
+            print("Cannot divide by zero!")
+        else:
+            print("Result:", num1 / num2)
+    else:
+        print("Invalid choice!")
+
+if __name__ == "__main__":
+    calculator()
